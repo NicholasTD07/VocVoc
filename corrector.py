6dd94cfa00de9e4a6d0dd5c29b10005b7ae95442
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-11-13 20:26:33 +0800

    Added the folder of corpuses with the file big.txt from norvig.com .

--- corrector.py
+++ corrector.py
@@ -15,37 +15,44 @@ from re import findall
 
 class SpellCorrector :
 
-    """ This is the class for the spelling corrector."""
+    """ This is the class for the spelling corrector.\nPass the file object to start it."""
 
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-    def __init__(self, wordFile) :
-        self.wordList = wordFile.read()
+    def __init__(self, wordFile=None) :
         self.wordModel = defaultdict(lambda : 1)
+        self.addCorpus(wordFile)
+
+    def addCorpus(self, wordFile=None) :
+        if wordFile is not None :
+            self.wordList = findall( r'[a-z]+', wordFile.read().lower() )
+            self.trainModel()
 
     def trainModel(self) :
+        wordList = self.wordList
         wordModel = self.wordModel
         for word in wordList :
             wordModel[word] += 1
+        self.wordList = [] # clear the wordList
 
     def editD1(self, word) : # D1 for Distance = 1
-        splits = [ ( word[:i] + word[i:] ) for i in range( len(word+1) ) ]
-        deletes = [ a + b[1:] for a, b in splits if b ]
-        transposes = [ a + b[1] + b[0] + b[2:] for a, b in splits if (len(b)>1) ]
-        replaces = [ a + c + b[1:] for a, b in splits for c in alphabet if b ]
-        inserts = [ a + c + b for a, b in splits for c in alphabet ]
+        splits = [ ( word[:i], word[i:] ) for i in range( len(word)+1 ) ]
+        deletes = [ a + b[1:] for (a, b) in splits if b ]
+        transposes = [ a + b[1] + b[0] + b[2:] for (a, b) in splits if (len(b)>1) ]
+        replaces = [ a + c + b[1:] for (a, b) in splits for c in self.alphabet if b ]
+        inserts = [ a + c + b for (a, b) in splits for c in self.alphabet ]
 
-        return set(deletes, transposes, replaces, inserts)
+        return set(deletes + transposes + replaces + inserts)
 
     def knownEditD2(self, word) :
         return set( e2 for e1 in self.editD1(word) for e2 in self.editD1(e1) if e2 in self.wordModel )
 
     def known(self, wordList) :
-        return set( word in wordList if word in self.wordModel )
+        return set( word for word in wordList if word in self.wordModel )
 
     def correct(self, word) :
-        candidates = known( [word] ) or known( self.editD1(word) ) or knownEditD2(word) or [word]
-        return max(candidates, key=self.wordModel.get
-
+        candidates = self.known( [word] ) or self.known( self.editD1(word) ) or self.knownEditD2(word) or [word]
+        #return max(candidates, key=self.wordModel.get) # only return the one with highest possibility.
+        return sorted(candidates, key=self.wordModel.get, reverse=True) # return a sorted list with decending order.
 
 
