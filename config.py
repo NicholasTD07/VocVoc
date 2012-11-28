d11755390ccb9c21cc89bee34916c99f8c4e9cdb
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-11-28 10:55:11 +0800

    Config.ini was rewritten by the config.py.

--- config.py
+++ config.py
@@ -5,9 +5,13 @@ This is the config part of VocVoc.
 
 
 import configparser
+from os.path import dirname, join as pJoin
 
 
 __config__ = 'config.ini'
+__dir__ = dirname(__file__)
+#__path__ = join(__dir__, __config__)
+__config__ = pJoin(__dir__, __config__)
 
 
 class Config :
@@ -17,10 +21,24 @@ class Config :
 
     def __init__(self) :
         self.config = configparser.ConfigParser()
-        self.config.read()
+        self.config.read(__config__)
+        self.getConfig()
 
     def getConfig(self) :
+        config = self.config
+        self.corpusDir = config['Path']['corpusDir']
+        self.pickleDir = config['Path']['pickleDir']
+
+    def setConfig(self, section, option, value) :
+        try :
+            self.config[section][option] = value
+            with open(__config__, 'w') as configFile :
+                self.config.write(configFile)
+        except TypeError as error :
+            pass
+
 
 if __name__ == '__main__':
 
     config = Config()
+    config.setConfig('Path', 'corpusDir', '')
