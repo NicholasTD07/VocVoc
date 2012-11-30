8c2a59d6fd9a9f85ce2425a8dc999b895c13f4a7
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-11-30 13:23:01 +0800

    Added the argumentParser, the debug option and a description to the VocVoc.

--- VocVoc.py
+++ VocVoc.py
@@ -10,14 +10,17 @@ import logging
 # logging.handlers
 from logging.handlers import TimedRotatingFileHandler
 
+# argparse
+from argparse import ArgumentParser
+
 # interface
 from interface import *
 
 
-def getLogger() :
+def getLogger(DEBUG=False) :
     # Create and set the logger.
     logger = logging.getLogger('VocVoc')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
     # Create a TimedRotatingFileHandler.
     fileHandler = TimedRotatingFileHandler('VocVoc.log', when='w0')
     # Only log things which could be wrong.
@@ -35,7 +38,20 @@ def getLogger() :
     logger.info('Created Logger.')
 
 def VocVoc() :
-    getLogger()
+    descriptionMsg = """
+            Put the input to a file and the list in the dialog.
+            If the input is a word whose sound file can be found, then the file will be played.
+            """
+    verboseMsg = 'Turn on the DEBUG level for logging to see what is going on with Phonon.'
+    argParser = ArgumentParser(description=descriptionMsg)
+    argParser.add_argument(
+                            '-v',
+                            '--verbose',
+                            help=verboseMsg,
+                            action='store_true'
+                            )
+    args = argParser.parse_args()
+    getLogger(args.verbose)
     logger = logging.getLogger('VocVoc')
     logger.info('Starting VocVoc.')
     App()
