dc5f773964ab77bbfccadf29f8298fa4848b04bd
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-12-01 09:51:17 +0800

    Added logging into Config part.

--- config.py
+++ config.py
@@ -4,9 +4,14 @@ This is the config part of VocVoc.
 """
 
 
-import configparser
+# configparser
+from configparser import ConfigParser
+
+# os.path
 from os.path import dirname, join as pJoin
 
+# logger
+from logging import getLogger
 
 __config__ = 'config.ini'
 __dir__ = dirname(__file__)
@@ -20,22 +25,34 @@ class Config :
     """
 
     def __init__(self) :
-        self.config = configparser.ConfigParser()
+        self.logger = getLogger('VocVoc.Config')
+        self.info = self.logger.info
+        self.warn = self.logger.warn
+        self.info('Initializing Config.')
+        self.config = ConfigParser()
         self.config.read(__config__)
         self.getConfig()
+        self.info('Config Initialized.')
 
     def getConfig(self) :
+        self.info('Getting configs into Config.')
         config = self.config
         self.corpusDir = config['Path']['corpusDir']
         self.pickleDir = config['Path']['pickleDir']
+        self.info('Config got.')
 
     def setConfig(self, section, option, value) :
+        msg = 'Trying to set the option : {} with the given value : {}.'.format(option, value)
+        self.info(msg)
         try :
             self.config[section][option] = value
             with open(__config__, 'w') as configFile :
                 self.config.write(configFile)
         except TypeError as error :
-            pass
+            self.warn(error)
+            self.warn('Option NOT set.')
+            return
+        self.info('Option set sucessfully.')
 
 
 if __name__ == '__main__':
