092c5216762a92df304782b51d786cd75b86e6cd
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-12-07 16:33:18 +0800

    Added misc.locateWord() function.

--- misc.py
+++ misc.py
@@ -3,11 +3,20 @@
 This file contains things shouldn't go into interface.py.
 """
 
-__all__ = ['flush']
+__all__ = ['flush', 'locateWord']
 
 def flush(filePath, text) :
     with open(filePath, 'a') as textFile :
         text = "".join( [text, '\n'] )
         textFile.write(text)
+
+def locateWord(filePath, word) :
+    locatedLines = list()
+    with open(filePath) as textFile :
+        lines = textFile.readlines()
+    for line in lines :
+        if word in line :
+            locatedLines.append(lines.index(line))
+    return locatedLines
 
 
